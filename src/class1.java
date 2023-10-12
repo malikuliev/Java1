495c9705e5270fa9dae487716fc010d4068d2c89
malikuliev <mahammad.alikuliev@gmail.com>
2023-10-12 19:37:15 +0400

    Facebook

--- src/class1.java
+++ src/class1.java
@@ -2,11 +2,7 @@
 public class class1 {
 
 	public static void main(String[] args) {
-		int age=25;
-		boolean hasJob=false;
-		char studentGrade='C';
-		double carPrice=25000;
-		String employeeName="Məlik";
+		String sebekeName="Facebook";
 		
 		
 	}
