cecebc84805c4d7bdc76d86206e5d03139b06439
malikuliev <mahammad.alikuliev@gmail.com>
2023-10-12 19:03:27 +0400

    Update class1.java

--- src/class1.java
+++ src/class1.java
@@ -2,11 +2,13 @@
 public class class1 {
 
 	public static void main(String[] args) {
-		int age=20;
-		boolean hasHome=true;
-		char studentGrade = 'A';
-		double productPrice=3.8;
-		String studentName="Yaqub";
+		int age=25;
+		boolean hasJob=false;
+		char studentGrade='C';
+		double carPrice=25000;
+		String employeeName="Məlik";
+		
+		
 	}
 
 }
