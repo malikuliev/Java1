d08595ec241605ec1aebb8f0b1f8c7aeb1632972
malikuliev <mahammad.alikuliev@gmail.com>
2023-10-16 20:50:40 +0400

    3 5

--- src/class1.java
+++ src/class1.java
@@ -2,8 +2,8 @@
 public class class1 {
 
 	public static void main(String[] args) {
-		for(int a=1; a<=30; a++) {
-			if (a%7==0) {
+		for(int a=1; a<=200; a++) {
+			if (a%3==0 || a%5==0) {
 				System.out.println(a);
 			}
 		}
