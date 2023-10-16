87f41249cd57af7331add7310d47d45cf8fbf982
malikuliev <mahammad.alikuliev@gmail.com>
2023-10-16 16:47:21 +0400

    Dərs Praktikası-1

--- src/class1.java
+++ src/class1.java
@@ -2,8 +2,33 @@
 public class class1 {
 
 	public static void main(String[] args) {
-		String sebekeName="Facebook";
-		System.out.println(sebekeName);
+//		short a=69;
+//		byte b=(byte)a;
+//		System.out.println(b);
+		
+//		short a=589;
+//		byte b=(byte)a;
+//		System.out.println(b);
+		
+//		long a=458;
+//		int b=(int)a;
+//		System.out.println(b);
+		
+//		long a=52523635483L;
+//		int b=(int)a;
+//		System.out.println(b);
+		
+//		double a=5632.6;
+//		float b=(float)a;
+//		System.out.println(b);
+		
+//		char a='D';
+//		int b=(int)a;
+//		System.out.println(b);
+		
+		int a=128;
+		char b=(char)a;
+		System.out.println(b);
 		
 		
 	}
