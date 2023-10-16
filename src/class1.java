ba23b0c4397a325c860e176747a115501a70cb71
malikuliev <mahammad.alikuliev@gmail.com>
2023-10-16 20:48:46 +0400

    cwt

--- src/class1.java
+++ src/class1.java
@@ -2,34 +2,11 @@
 public class class1 {
 
 	public static void main(String[] args) {
-//		short a=69;
-//		byte b=(byte)a;
-//		System.out.println(b);
-		
-//		short a=589;
-//		byte b=(byte)a;
-//		System.out.println(b);
-		
-//		long a=458;
-//		int b=(int)a;
-//		System.out.println(b);
-		
-//		long a=52523635483L;
-//		int b=(int)a;
-//		System.out.println(b);
-		
-//		double a=5632.6;
-//		float b=(float)a;
-//		System.out.println(b);
-		
-//		char a='D';
-//		int b=(int)a;
-//		System.out.println(b);
-		
-		int a=128;
-		char b=(char)a;
-		System.out.println(b);
-		
+		for(int a=1; a<=100; a++) {
+			if (a%2==0) {
+				System.out.println(a);
+			}
+		}
 		
 	}
 
