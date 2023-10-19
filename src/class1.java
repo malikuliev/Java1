b9218df5acd3e6597dd3227e722490cb9936af39
malikuliev <mahammad.alikuliev@gmail.com>
2023-10-19 19:56:52 +0400

    Sade eded

--- src/class1.java
+++ src/class1.java
@@ -1,13 +1,6 @@
-
-public class class1 {
-
+public class Main
+{
 	public static void main(String[] args) {
-		for(int a=1; a<=200; a++) {
-			if (a%3==0 || a%5==0) {
-				System.out.println(a);
-			}
-		}
 		
 	}
-
 }
